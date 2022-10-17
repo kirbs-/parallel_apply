810e0e5b6a132f75d864e28b776166a51cb2f32b
Chris Kirby <chris.kirby@asurion.com>
2022-10-16 20:04:07 -0400

    Fixed passing module as function

--- parallel_apply/__init__.py
+++ parallel_apply/__init__.py
@@ -14,29 +14,55 @@ except ImportError:  # pragma: no cover
         from pandas.core.groupby import DataFrameGroupBy, SeriesGroupBy
 
 from tqdm import tqdm
+import tempfile, itertools, inspect
+import os
+
+
+tempfile._get_candidate_names = lambda: itertools.repeat('papply') 
+
 
 def p_apply(df, func, *args, **kwargs):
-    n_jobs = kwargs.get('n_jobs', mp.cpu_count() - 3)
+    n_jobs = kwargs.get('n_workers', mp.cpu_count() - 3)
     chunksize = kwargs.get('chunksize', 50)
     out = []
-    with mp.Pool(processes=n_jobs) as p:
-        with tqdm(total=len(df)+1) as pbar:
-            for _ in p.imap_unordered(func, [group for idx, group in df], chunksize=chunksize):
-                out.append(_)
-                pbar.update()
-    return out
+
+    func_name = func.__name__
+    with tempfile.NamedTemporaryFile(mode='w+', prefix='', suffix='.py', dir=os.getcwd()) as f:
+        f.write(inspect.getsource(func))
+        f.flush()
+
+        import papply
+        _func = getattr(papply, func_name)
+        if inspect.getmodule(func).__name__ != '__main__':
+            _func = func
+        with mp.Pool(processes=n_jobs) as p:
+            with tqdm(total=len(df)) as pbar:
+                for _ in p.imap_unordered(_func, df, chunksize=chunksize):
+                    out.append(_)
+                    pbar.update()
+    return Series(out)
+    
 
 def row_apply(df, func, *args, **kwargs):
-    n_jobs = kwargs.get('n_jobs', mp.cpu_count() - 3)
+    n_jobs = kwargs.get('n_workers', mp.cpu_count() - 3)
     chunksize = kwargs.get('chunksize', 50)
     out = []
     # func = functools.partial(func, *args, **kwargs)
-    with mp.Pool(processes=n_jobs) as p:
-        with tqdm(total=len(df)+1) as pbar:
-            # for _ in p.imap_unordered(func, [group for idx, group in df.iterrows()], chunksize=chunksize):
-            for _ in p.imap(func, _iter(df), chunksize=chunksize):
-                out.append(_)
-                pbar.update()
+    func_name = func.__name__
+    with tempfile.NamedTemporaryFile(mode='w+', prefix='', suffix='.py', dir=os.getcwd()) as f:
+        f.write(inspect.getsource(func))
+        f.flush()
+
+        import papply
+        _func = getattr(papply, func_name)
+        if inspect.getmodule(func).__name__ != '__main__':
+            _func = func
+        with mp.Pool(processes=n_jobs) as p:
+            with tqdm(total=len(df)) as pbar:
+                # for _ in p.imap_unordered(func, [group for idx, group in df.iterrows()], chunksize=chunksize):
+                for _ in p.imap(_func, _iter(df), chunksize=chunksize):
+                    out.append(_)
+                    pbar.update()
     return Series(out)
 
 def _iter(df):
